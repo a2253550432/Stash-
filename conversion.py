d5c023d46cf2c7806952fc5cf6e35b0a490ceb49
Kevin <a2253550432@163.com>
2024-09-01 22:47:15 +0800

    added conversion.py

---
+++ conversion.py
@@ -0,0 +1,124 @@
+import yaml
+import requests
+import re
+import base64
+from urllib.parse import urlparse, parse_qs, unquote
+
+def get_servers():
+    # 解析trojan url，生成字典，包含服务器内容
+    url = 'http://154.17.18.53/api/v1/client/subscribe?token=222fe0e1c9d72108589523c332b9cb3b'
+
+    content = requests.get(url).content
+
+    content = base64.b64decode(content).decode()
+    all_samples = content.split('\r\n')
+    ret = []
+    # Trojan URL
+    for samp in all_samples:
+        # 解析URL
+        if samp:
+            parsed_url = urlparse(samp)
+
+            # 提取各部分信息
+            protocol = parsed_url.scheme  # 协议类型
+            password = parsed_url.username  # 用户ID
+            server = parsed_url.hostname  # 服务器地址
+            port = parsed_url.port  # 端口号
+            params = parse_qs(parsed_url.query)  # 解析查询参数
+            name = unquote(parsed_url.fragment)  # 解码服务器名称
+
+            # 输出解析结果
+            # print(f"协议类型: {protocol}")
+            # print(f"用户ID: {userinfo}")
+            # print(f"服务器地址: {host}")
+            # print(f"端口号: {port}")
+            # print(f"查询参数: {params}")
+            # print(f"备注: {name}")
+            result = {}
+            result['name'] = name
+            result['type'] = protocol
+            result['server'] = server
+            result['port'] = port
+            result['password'] = password
+            for key in params:
+                if key=='allowInsecure':
+                    val = params[key][0]
+                    if val=='0':
+                        result['udp']=True
+                    else:
+                        result['udp']=False
+                if key=='sni':
+                    val = params[key][0]
+                    result['sni'] = val
+            ret.append(result)
+    return ret
+
+
+servers = get_servers()
+
+with open("my.yaml",'r',encoding='utf-8') as file:
+    datamy = yaml.safe_load(file)
+
+
+datamy['proxies'] = servers
+
+countries = ['香港','日本','韩国','台湾','新加坡','美国','其他']
+all_proxies=[]
+countries = dict.fromkeys(countries)
+for i in countries:
+    countries[i]=[]
+
+for proxy in datamy['proxies']:
+    get = 0
+    for country in countries:
+        if country in proxy['name']:
+            countries[country].append(proxy['name'])
+            get = 1
+            break
+    if get==0:
+        countries['其他'].append(proxy['name'])
+    all_proxies.append(proxy['name'])
+
+proxy_group = []
+selection = datamy['proxy-groups'][0]
+proxy_group.append(selection)
+
+# 加入手动切换 和 自动选择 两个group
+manual,auto = {},{}
+manual['name'] = '手动切换'
+manual['type'] = 'select'
+manual['proxies'] = all_proxies
+
+auto['name'] = '自动选择'
+auto['type'] = 'url-test'
+auto['proxies'] = all_proxies
+
+proxy_group.append(manual)
+proxy_group.append(auto)
+
+# 加入各国节点列表
+
+for country in countries:
+    group = {}
+    group['name'] = country
+    group['type'] = 'url-test'
+    group['proxies'] = countries[country]
+    proxy_group.append(group)
+
+# 加入openai等特定分类
+group = {}
+group['name'] = 'openai'
+group['type'] = 'select'
+group['proxies'] = datamy['proxy-groups'][0]['proxies'][1:] # 去除香港
+proxy_group.append(group)
+
+datamy['proxy-groups'] = proxy_group
+
+
+
+with open('output.yaml', 'w', encoding='utf-8') as file:
+    yaml.dump(datamy, file, allow_unicode=True, sort_keys=False)
+
+
+
+
